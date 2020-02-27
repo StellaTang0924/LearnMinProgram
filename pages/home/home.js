383d239a0f8af9c8a4de6e4426f3bb7907bf1546
Stella <936743539@qq.com>
2020-02-27 20:52:39 +0800

    全局配置小程序

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,29 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:"coderwhy",
+    age:18,
+    students:[
+      {id:1,name:"aa",age:10},
+      {id:2,name:"bb",age:11},
+      {id:3,name:"cc",age:12},
+      {id:4,name:"dd",age:13},
+      {id:5,name:"ee",age:14}
+    ],
+    conter:0
   },
+  handleBtnClick(){
+    // 该做法界面不会刷新
+    // this.data.conter += 1
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // this.setData()
+    this.setData({
+      conter:this.data.conter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      conter: this.data.conter - 1
+    })
   }
 })
