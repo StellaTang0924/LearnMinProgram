31a286d6a5a0ac38ee44c42aaa0086a03f9f297d
Stella <936743539@qq.com>
2020-03-03 17:01:55 +0800

    小程序转发

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,41 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中ing',
+      duration:3000,
+      icon:'loading',
+      mask:true
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容，哈哈哈哈哈哈！',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载ing',
+    })
 
+    setTimeout(()=>{
+      wx.hideLoading()
+    },1000)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage:function(options){
+    return{
+      title:"你好啊，赵春歌",
+      path: "pages/home/home"
+    }
   }
 })
